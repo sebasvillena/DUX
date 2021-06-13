4f65191f547d430348d1e8e7c0ca40e215d67e7d
Sebastian Villena <sebastia.villena@gmail.com>
2021-06-13 19:29:00 -0300

    Estructura basica de la app

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # DUX
+Repo para hacer el proyecto de DUX
+
+## Installation
+Usar npm para para instalar las dependencias
+```bash
+npm install
+```
+## Run
+```bash
+node app.js
+```
+Si eso no funciona, es porque no agregué las dependecias correctamente, ejecutar:
+```bash
+npm install express
+```
---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = new express();
+const path = require('path');
+
+
+const hostname = '127.0.0.1';
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
