ee415e2fd1e1a26decbc1a8e8042e735edd672fc
Sebastian Villena <sebastia.villena@gmail.com>
2021-10-06 22:30:44 -0300

    fixed port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 
 const hostname = '127.0.0.1';
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "/static"))
 app.get('/', (req, res) => {
